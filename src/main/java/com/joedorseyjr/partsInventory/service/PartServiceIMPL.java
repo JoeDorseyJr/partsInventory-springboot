e8f7c354176c0656a8b3682fc732c1ef845a1cb0
Joe Dorsey <joedorseyjr@gmail.com>
2022-01-12 00:39:50 -0500

    ADD: Autowired annotation

--- src/main/java/com/joedorseyjr/partsInventory/service/PartServiceIMPL.java
+++ src/main/java/com/joedorseyjr/partsInventory/service/PartServiceIMPL.java
@@ -2,6 +2,8 @@ package com.joedorseyjr.partsInventory.service;
 
 import com.joedorseyjr.partsInventory.DAO.PartDAO;
 import com.joedorseyjr.partsInventory.entity.Part;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,7 +12,8 @@ import java.util.List;
 public class PartServiceIMPL implements PartService{
     private final PartDAO partDAO;
 
-    public PartServiceIMPL(PartDAO partDAO) {
+    @Autowired
+    public PartServiceIMPL( PartDAO partDAO) {
         this.partDAO = partDAO;
     }
 
